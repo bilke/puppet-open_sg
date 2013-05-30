a4f57eddb51f0bbef570e367748578739dc0f372
Lars Bilke <larsbilke83@googlemail.com>
2013-05-30 11:32:38 +0300

    Added png support.

--- files/brews/open-sg.rb
+++ files/brews/open-sg.rb
@@ -6,15 +6,16 @@ class OpenSg < Formula
   head 'https://github.com/bilke/OpenSG-1.8.git'
   homepage 'http://www.opensg.org/wiki'
 
-  version "1.8-boxen1"
+  version "1.8-boxen2"
 
   depends_on 'libtiff'
   depends_on 'jpeg'
+  depends_on 'libpng'
 
   def install
     ENV.deparallelize
     ENV.no_optimization
-    system "./configure", "--prefix=#{prefix}", "--enable-glut", "--enable-tif", "--enable-jpg"
+    system "./configure", "--prefix=#{prefix}", "--enable-glut", "--enable-tif", "--enable-jpg", "--enable-png"
     Dir.chdir 'Builds/i386-apple-darwin-g++'
     system "make opt"
     system "make install"
